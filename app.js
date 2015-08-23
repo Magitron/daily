8892b36292bb5a7d1cc54684153a56bef835fe7d
Magitron <maggiecaspar@gmail.com>
2015-08-22 19:36:07 -0700

    hello world

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-app.js
+var koa = require('koa');
+var app = koa();
+var router = require('koa-router')();
+var logger = require('koa-logger');
+var serve = require('koa-static');
+var router = require('koa-router')();
+ 
+// Logger
+app.use(logger());
+app.use(serve(process.cwd() + '/public'));
+
+
+router.get('/', function *(next) {
+  this.body = 'Hello World!';
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+
+if (!module.parent) {
+  app.listen(1337);
+  console.log('listening on port 1337');
+}
